bd62516c41770d7e40bf95055b1909ca49549f82
Rafael Coelho <79323806+CoelhoRafael@users.noreply.github.com>
2023-11-24 01:56:08 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação da plataforma tabnews pelo curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Eu comi coco e gostei</h1>
+function Home() {
+  return <h1>Eu comi coco e gostei</h1>;
 }
 
+function teste() {
+  return "pqp";
+}
 export default Home;
