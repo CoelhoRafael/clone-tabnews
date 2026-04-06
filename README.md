6f5595219181d6aff207247e837b6d58223c1c50
Rafael Coelho <79323806+CoelhoRafael@users.noreply.github.com>
2026-04-06 00:41:31 +0000

    editor config and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This is a repositorie to clone the tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Teste</h1>
+function Home() {
+  return <h1>Teste</h1>;
+}
+
+function teste() {
+  return "teetete";
 }
 
 export default Home;
